4609d4aa3acfca1b9155889fca7efccb1633768f
Stian Selnes <stian@pexip.com>
2016-09-16 10:34:26 +0200

    Remove external build dependencies

--- autogen.sh
+++ autogen.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 
+test -n "$srcdir" || srcdir=`dirname "$0"`
+test -n "$srcdir" || srcdir=.
+
 autoreconf --force --install --verbose || exit $?
 test -n "$NOCONFIGURE" || "$srcdir/configure" "$@"
